65c43120737c7241af365e7a02f99b18716d67be
Haben <haben7@gmail.com>
2020-03-23 23:12:05 -0700

    added method to find permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,36 @@
 
+
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  first_hash = {}
+  second_hash = {}
+ 
+  string1.chars.each do |letter|
+    first_hash[letter] = 0
+  end
+
+  string1.chars.each do |letter|
+   first_hash[letter] += 1
+  end
+
+  string2.chars.each do |letter|
+    second_hash[letter] = 0
+  end
+
+  string2.chars.each do |letter|
+    second_hash[letter] += 1
+   end
+
+   first_hash.each do |key, value|
+    if second_hash.has_key?(key) 
+      if second_hash[key] == value
+
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+  return true
 end
+
