d024d77c14089033e9b4b44557dafffdf03139ac
Haben <haben7@gmail.com>
2020-03-23 23:11:31 -0700

    added a method to find intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,19 @@
-def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+#Design and implement a method that takes two integer arrays with unique 
+#values and returns their intersection in a new array.
+def intersection(array1, array2)
+  intersection = []
+  new_hash = {}
+  
+  array1.each do |number1|
+    new_hash[number1] = 1
+  end
+
+  array2.each do |number2|
+    if new_hash[number2] == 1
+      intersection << number2
+    end
+  end
+  return intersection
+    
 end
+
